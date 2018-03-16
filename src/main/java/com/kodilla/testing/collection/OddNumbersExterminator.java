06bff16253892297dbbdbb51915625e882c1fc16
“adam <adam2budny@gmail.com>
2018-03-16 21:43:44 +0100

    OddNumbersExterminator

--- src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -3,14 +3,16 @@ package com.kodilla.testing.collection;
 import java.util.ArrayList;
 
 public class OddNumbersExterminator {
-    public void exterminate(ArrayList<Integer> numbers){
-        int eachValue=0;
-        for (int i=0; i<numbers.size();i++){
-            eachValue=numbers.get(i);
-            if (eachValue %2==0){
-                System.out.println(eachValue);
+
+    public ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
+        ArrayList<Integer> evenNumbers = new ArrayList<Integer>();
+        int eachValue = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            eachValue = numbers.get(i);
+            if (eachValue % 2 == 0) {
+                evenNumbers.add(eachValue);
             }
         }
+        return evenNumbers;
     }
 }
-
--- src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -22,30 +22,28 @@ public class CollectionTestSuite {
         System.out.println("Test Suite: end");
     }
     @Test
-    public void testOddNumbersExterminatorEmptyList(){
+    public void testOddNumbersExterminatorEmptyList() {
+        ArrayList<Integer> theList = new ArrayList<Integer>();
+        Random generator = new Random();
+        for (int n = 0; n < 100; n++) {
+            theList.add(generator.nextInt(50));
+        }
+        ;
+        int result = theList.size();
+        if (result == 0) {
+            System.out.println("Error");
+        } else {
+            System.out.println("List contain:" + theList);
+        }
+    }
+    @Test
+    public void testOddNumbersExterminatorNormalList(){
         ArrayList<Integer> theList = new ArrayList<Integer>();
         Random generator = new Random();
         for (int n=0; n<100; n++){
-            theList.add(generator.nextInt(50));
-        };
+            theList.add(generator.nextInt(50)); };
         int result = theList.size();
-        Assert.assertEquals(result==0) /*if(){
-            System.out.println("Error");
-        }else{
-            System.out.println("List contain:"+theList);
-        }*/
-    public void testOddNumbersExterminatorNormalList(){
-            ArrayList<Integer> theList = new ArrayList<Integer>();
-            Random generator = new Random();
-            for (int n=0; n<100; n++){
-                theList.add(generator.nextInt(50));
-            };
-            int result = theList.size();
-            ArrayList<theList> evenList = new ArrayList<theList>();
-            evenList.exterminate(theList);
-            Assert.assertEquals
+       // ArrayList<Integer> evenList = new ArrayList<Integer>();
+        ArrayList<Integer> evenList = theList.exterminate(ArrayList<Integer>);
         }
-
-
     }
-}
