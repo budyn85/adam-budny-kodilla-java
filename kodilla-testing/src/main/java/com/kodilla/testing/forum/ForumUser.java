2c739f09a1e79374a62481f99d7ce8f20941368c
“adam <adam2budny@gmail.com>
2018-03-17 20:45:27 +0100

    zadanie 6.4

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumUser.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumUser.java
@@ -13,40 +13,56 @@ public class ForumUser {
         this.name = name;
         this.realName = realName;
     }
-    public void addPost(String author, String postBody){
 
-    }
-    public void addComment(ForumPost thePost,String author,String commentBody){
-
-    }
-    public int getPostsQuantity(){
-        // return 100 temporarily
-        return 100;
+    public int getCommentsQuantity() {
+        return comments.size();
     }
 
-    public int getCommentsQuantity(){
-        // return 100 temporarily
-        return 100;
+    public void addComment(ForumPost thePost, String author, String commentBody) {
+        ForumComment theComment = new ForumComment(thePost, commentBody, author);
+        comments.add(theComment);
+    }
+    public int getPostsQuantity() {
+        return posts.size();
     }
 
-    public ForumPost getPost(int postNumber){
-        // returning null means that the operation was unsuccessful
-        return null;
+    public void addPost(String author, String postBody) {
+        ForumPost thePost = new ForumPost(postBody, postBody);
+        posts.add(thePost);
     }
 
-    public ForumComment getComment(int commentNumber){
-        // returning null means that the operation was unsuccessful
-        return null;
+    public ForumPost getPost(int postNumber) {
+        ForumPost thePost = null;
+        if (postNumber >= 0 && postNumber < posts.size()) {
+            thePost = posts.get(postNumber);
+        }
+        return thePost;
     }
 
-    public boolean removePost(ForumPost thePost){
-        // return true temporarily
-        return true;
+    public ForumComment getComment(int commentNumber) {
+        ForumComment theComment = null;
+        if (commentNumber >= 0 && commentNumber < comments.size()){
+            theComment = comments.get(commentNumber);
+        }
+        return theComment;
     }
 
-    public boolean removeComment(ForumComment theComment){
-        // return true temporarily
-        return true;
+    public boolean removePost(ForumPost thePost) {
+        boolean result = false;
+        if (posts.contains(thePost)){
+            posts.remove(thePost);
+            result = true;
+        }
+        return result;
+    }
+
+    public boolean removeComment(ForumComment theComment) {
+        boolean result = false;
+        if (comments.contains(theComment)){
+            comments.remove(theComment);
+            result = true;
+        }
+        return result;
     }
 
     public String getName() {
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -0,0 +1,66 @@
+package com.kodilla.testing.shape;
+
+import org.junit.*;
+
+public class ShapeCollectorTestSuite {
+        private static int testCounter = 0;
+
+        @BeforeClass
+        public static void beforeAllTests() {
+            System.out.println("This is the beginning of tests.");
+        }
+
+        @AfterClass
+        public static void afterAllTests() {
+            System.out.println("All tests are finished.");
+        }
+
+        @Before
+        public void beforeEveryTest() {
+            testCounter++;
+            System.out.println("Preparing to execute test #" + testCounter);
+        }
+    }
+    //sprawdza czy dodano figure
+    @Test
+    public void testAddFigure() {
+        //Given
+        ShapeCollector shapeCollector = new ShapeCollector();
+
+
+        //When
+        shapeCollector.addFigure()( "Triangle");
+
+        //Then
+        Assert.assertEquals(1, figureList.size());
+
+
+    //sprawdza czy usunieto figure
+
+        @Test
+        public void testRemove() {
+            //Given
+            ShapeCollector shapeCollector = new ShapeCollector();
+
+            //When
+            boolean result = shapeCollector.remove(0);
+
+            //Then
+            Assert.assertTrue(result);
+            Assert.assertEquals(0, shapeCollector.size());
+        }
+    // sprawdza czy pobrano figure z n miejsca
+        @Test
+        public void testGetFigure() {
+            //Given
+            ShapeCollector shapeCollector = new ShapeCollector();
+
+            //When
+            boolean result = shapeCollector.get(2);
+
+            //Then
+            Assert.assertTrue(result);
+            Assert.assertEquals(2, shapeCollector.size());
+
+    // sprawdza czy usunieto figure z n miejsca
+}
