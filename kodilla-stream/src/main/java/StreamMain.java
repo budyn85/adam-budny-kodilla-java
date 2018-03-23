237a8a540fe070382a204cd4b1e93a496ca3f964
“adam <adam2budny@gmail.com>
2018-03-23 22:41:14 +0100

    Zadanie 7.2

--- kodilla-stream/src/main/java/StreamMain.java
+++ kodilla-stream/src/main/java/StreamMain.java
@@ -1,6 +1,5 @@
-
-
 import beautifier.PoemBeautifier;
+import iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -9,5 +8,9 @@ public class StreamMain {
         String upper =poemBeautifier.beautify("L", (text) -> text);
         System.out.println(print);
         System.out.println(upper);
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
+
 }
