b8f124fa4281693c5c189bcf5eeaef8ba4929e88
“adam <adam2budny@gmail.com>
2018-04-18 21:00:49 +0200

    Zadanie 11.1

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,21 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LoggerTestSuite {
+    @BeforeClass
+    public static void logLogger(){
+        Logger.getInstance().log("My first log");
+    }
+
+    @Test
+    public void testGetLastLog(){
+        //Given
+        //When
+        String result=Logger.getInstance().getLastLog();
+        //Then
+        Assert.assertEquals("My first log",result);
+    }
+}
